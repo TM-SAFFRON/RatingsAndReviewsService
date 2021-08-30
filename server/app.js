cc0ab329256868ef31d1bb17c614728afc7ec1c4
Shirley Zhang <shirley.a.zhang@gmail.com>
2021-08-30 16:02:11 -0700

    clean up

--- server/app.js
+++ server/app.js
@@ -4,21 +4,15 @@ const controllers = require('./controllers');
 const app = express();
 module.exports.app = app;
 
-// app.set('port', 5000);
-
 app.use(express.json());
-
+app.get('/', () => {
+  res.status(200)send('Welcome to Ratings and Reviews!');
+})
 app.get('/reviews', controllers.reviews.get);
 app.get('/reviews/meta', controllers.reviews.getMeta);
 app.post('/reviews', controllers.reviews.post);
 app.put('/reviews/:review_id/helpful', controllers.reviews.putHelpful);
 app.put('/reviews/:review_id/report', controllers.reviews.putReport);
-
-// how to serve client on another repo?
-// app.use(express.static(__dirname + '/../client'));
-
-// app.listen(app.get('port'));
-// console.log('Listening on', app.get('port'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log('Listening on port', PORT));
--- server/db/index.js
+++ server/db/index.js
@@ -1,22 +1,4 @@
-// const postgres = require('postgresql');
 const config = require('../../config.js');
 const { Client } = require('pg');
 
 module.exports = new Client(config);
-
-// module.exports.client;
-
-// const psqlConnection = postgres.createConnection({
-//   host: 'localhost',
-//   user: 'postgres',
-//   password,
-//   database: 'ratingsandreviews'
-// })
-
-// psqlConnection.connect((err) => {
-//   if (err) {
-//     throw err
-//   } else {
-//     console.log('connected to database');
-//   }
-// });
